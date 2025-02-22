2f20291580c23ce7e7a0b9ef1c0a696eede56bec
shuya17 <shuya.2030@gmail.com>
2025-02-22 17:40:19 +0900

    flexGrowの削除

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,24 +1,21 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import { Typography } from "@mui/material";
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ bgcolor: "white" }}>
-        <Toolbar sx={{ color: "#E45C92" }}>
-          <Typography
-            align={"center"}
-            width={"100%"}
-            fontFamily={"serif"}
-            fontSize={{ xs: 25, md: 40 }}
-          >
-            Yusei's Graduate web
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static" sx={{ bgcolor: "white" }}>
+      <Toolbar sx={{ color: "#E45C92" }}>
+        <Typography
+          align={"center"}
+          width={"100%"}
+          fontFamily={"serif"}
+          fontSize={{ xs: 25, md: 40 }}
+        >
+          Yusei's Graduate web
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
