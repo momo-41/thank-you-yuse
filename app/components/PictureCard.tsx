329f9f44d72e6b08b8de2177ba6ef48c69bc3180
umeryo34 <ryo34ume@icloud.com>
2025-03-01 10:26:02 +0900

    CSSの修正

--- app/components/PictureCard.tsx
+++ app/components/PictureCard.tsx
@@ -1,40 +1,56 @@
-import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+"use client";
+
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
 const PictureCard = () => {
+  const isMobile = useMediaQuery("(max-width:600px)");
   return (
-    <Card
-      sx={{
-        width: 450,
-        height: 400,
-        paddingTop: 1.5,
-        paddingLeft: 1.5,
-        paddingRight: 1.5,
-        bgcolor: "#f8f8f8",
-      }}
-    >
-      <CardMedia sx={{ height: 350, width: "100%" }} image="/yuse.jpg" />
-      <CardContent
+    <Box marginLeft={isMobile ? 3 : 0} marginRight={isMobile ? 6 : 0}>
+      <Card
         sx={{
-          display: "flex",
-          justifyContent: "space-between",
+          width: isMobile ? "100%" : 600,
+          height: isMobile ? 250 : 450,
+          paddingTop: 1.5,
+          paddingLeft: 1.5,
+          paddingRight: 1.5,
+          bgcolor: "#f8f8f8",
+          marginTop: 5,
         }}
       >
-        <Box display={"flex"} alignItems={"center"}>
-          <ChatBubbleOutlineIcon />
-          <Typography fontSize={11} fontFamily={"serif"} marginLeft={1}>
-            株式会社PeachTech宣材写真
-          </Typography>
-        </Box>
-        <Box display={"flex"} alignItems={"center"}>
-          <Typography fontSize={11} marginRight={1} fontFamily={"serif"}>
-            Picture More
-          </Typography>
-          <ArrowRightAltIcon />
-        </Box>
-      </CardContent>
-    </Card>
+        <CardMedia
+          sx={{ height: isMobile ? 200 : 400, width: "100%" }}
+          image="/yuse.jpg"
+        />
+        <CardContent
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box display={"flex"} alignItems={"center"}>
+            <ChatBubbleOutlineIcon />
+            <Typography fontSize={11} fontFamily={"serif"} marginLeft={1}>
+              株式会社PeachTech宣材写真
+            </Typography>
+          </Box>
+          <Box display={"flex"} alignItems={"center"}>
+            <Typography fontSize={11} marginRight={1} fontFamily={"serif"}>
+              Picture More
+            </Typography>
+            <ArrowRightAltIcon />
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 export default PictureCard;
