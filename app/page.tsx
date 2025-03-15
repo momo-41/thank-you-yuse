daad77ca5e3b7e1badd006dc7b9d4ad258b987f8
shuya17 <shuya.2030@gmail.com>
2025-03-15 15:45:44 +0900

    GridCardとYearGridCardのimport文の作成 データ例の追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Header from "./components/Header";
 import FirstView from "./components/FirstView";
-import GridCard from "./components/GridCard";
-import YearGridCard from "./components/YearGridCard";
 import Tabpanel from "./components/Tabpanel";
 
 export default function Home() {
