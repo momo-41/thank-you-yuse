55896fe805fcd5f8268bc057fadc3b54c937d20e
umeryo34 <ryo34ume@icloud.com>
2025-03-16 19:40:33 +0900

    画面の大きさに幅と高さが対応するようにCSSを修正

--- app/components/GalleryThirdRow.tsx
+++ app/components/GalleryThirdRow.tsx
@@ -1,17 +1,25 @@
 import { Box } from "@mui/material";
-import PictureImage from "./GalleryItem";
+import GalleryItem from "./GalleryItem";
+import { GalleryRowProps } from "../types/types";
 
 const GalleryThirdRow: React.FC<GalleryRowProps> = ({ images }) => {
   return (
-    <Box display="flex" gap={1} m={1} flexWrap="nowrap" justifyContent="center">
-      {images.slice(0, 4).map((src, index) => (
-        <PictureImage
-          key={index}
-          src={src}
-          width={index === 1 ? 400 : 300}
-          height={200}
-        />
-      ))}
+    <Box width={"100%"}>
+      <Box
+        display="flex"
+        gap={1}
+        m={1}
+        flexWrap="nowrap"
+        justifyContent="center"
+      >
+        {images.slice(0, 4).map((src, index) => (
+          <GalleryItem
+            key={index}
+            src={src}
+            width={index === 1 ? "31%" : "23%"}
+          />
+        ))}
+      </Box>
     </Box>
   );
 };
