429d3e5aad046670dc36db2b716439257fe2fa17
umeryo34 <ryo34ume@icloud.com>
2025-03-16 19:41:47 +0900

    幅が広がった時に高さも広がるように修正

--- app/components/GalleryItem.tsx
+++ app/components/GalleryItem.tsx
@@ -1,18 +1,13 @@
 import { Box } from "@mui/material";
 import Image from "next/image";
+import { GalleryItemProps } from "../types/types";
 
-type PictureImageProps = {
-  src: string;
-  width: number;
-  height: number;
-};
-
-const PictureImage = ({ src, width, height }: PictureImageProps) => (
+const GalleryItem = ({ src, width }: GalleryItemProps) => (
   <Box
     position="relative"
     overflow="hidden"
     width={{ xs: "calc(100% / 3 - 8px)", md: width }}
-    height={{ xs: 150, md: height }}
+    height={{ xs: 150, md: 200, xl: 300 }}
   >
     <Image
       src={src}
@@ -24,4 +19,4 @@ const PictureImage = ({ src, width, height }: PictureImageProps) => (
   </Box>
 );
 
-export default PictureImage;
+export default GalleryItem;
