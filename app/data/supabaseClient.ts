182bac374cea0fa9968aa8f6d52a2d075711bdd6
yoshiddddd <yoshidakerapo@gmail.com>
2025-03-16 18:18:36 +0900

    test

--- app/data/supabaseClient.ts
+++ app/data/supabaseClient.ts
@@ -8,3 +8,4 @@ const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+
