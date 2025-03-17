9eff13b1ff421019b7bc3b2e168f8572eb4d0682
momo-41 <minimizu41@gmail.com>
2025-03-17 18:27:12 +0900

    ファイル名の変更とインデントの修正

---
+++ app/data/supabase-client.ts
@@ -0,0 +1,16 @@
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+
+if (
+  !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+) {
+  throw new Error("Missing Supabase environment variables");
+}
+
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase: SupabaseClient = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
